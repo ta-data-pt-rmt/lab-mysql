648b9d142e168c1fc183a78480b1857a085c123b
Ames <amelie.haberland@ironhack.com>
2022-10-28 22:32:16 +0100

    Amelie

---
+++ createandseed.sql
@@ -0,0 +1,36 @@
+/*I created the tables manually with the tool section*/
+Insert Into car_dealership.cars(VIN, manufacturer, model, years, color)
+values 
+("K096I98581DHSNUP", "Volkswagen", "Tiguan", "2019", "Blue"),
+("ZM8G7BEUQZ97IH46V", "Peugeot", "Rifter", "2019", "Red"),
+("RKXVNNIHLVVZOUB4M", "Ford", "Fusion", "2018", "White"),
+("HKNDGS7CU31E9Z7JW", "Toyota", "RAV4", "2018", "Silver"),
+("DAM41UDN3CHU2WVF6", "Volvo", "V60", "2019", "Gray");
+
+Insert Into car_dealership.customers("customer ID", "name", "phone number", email,
+ address, city, state, country, "zip code")
+values 
+("10001", "Pablo Picasso", "+34 636 17 63 82", "-", "Paseo de la Chopera, 14", "Madrid",
+"Madrid", "Spain", "28045"),
+("20001", "Abraham Lincoln", "+1 305 907 7086", "-", "120 SW 8th St", "Miami",
+"Florida", "United States", "33130"),
+("30001", "Napoleon Bonaparte", "+33 1 79 75 40 00", "-",
+"40 Rue du Colisée", "Paris", "Île-de-France", "France", "75008");
+/* why does the customer ID not work? I can´t figure it out*/
+
+Insert into car_dealership.salesperson("staff ID", "name", store)
+values
+("00001", "Petey Cruiser", "Madrid"),
+("00002", "Anna Sthesia", "Barcelona"),
+("00003", "Paul Molive", "Berlin"),
+("00004", "Gail Forcewind", "Paris"),
+("00005", "Paige Turner", "Mimia"),
+("00006", "Bob Frapples", "Mexico City"),
+("00007", "Walter Melon", "Amsterdam"),
+("00008", "Shonda Leer", "São Paulo");
+
+Insert into car_dealership.invoices(invoices nb, date, VIN, customer ID, staff ID)
+values
+("852399038", "22-08-2018", "0", "1", "3"),
+("731166526", "31-12-2018" "3", "0", "5"),
+("271135104", "22-01-2019", "2", "2", "7")
